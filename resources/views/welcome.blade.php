02ed622bdf81606faf164ce59d901f2b88a61ab8
Angus <angus.goody@gmail.com>
2021-03-27 15:53:50 +0000

    Initial files

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<x-guest-layout>
+  <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+    <div class="sm:text-center lg:text-left">
+      <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+        <span class="block xl:inline">Welcome to my</span>
+        <span class="block text-blue-600 xl:inline">website</span>
+      </h1>
+      <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+      </p>
+      <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+        <div class="rounded-md shadow">
+          <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10">
+            Projects
+          </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
+            Contact
+          </a>
+        </div>
+      </div>
+    </div>
+  </main>
+</x-guest-layout>
