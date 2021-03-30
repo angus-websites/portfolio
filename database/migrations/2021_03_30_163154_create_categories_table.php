885b9b8cc5d9a2cd1848b3d502d5673935f73b2c
Angus <angus.goody@gmail.com>
2021-03-30 17:41:17 +0100

    Created Category model and seeded

---
+++ database/migrations/2021_03_30_163154_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            //Fields
+            $table->text('name');
+            $table->text('short_name');
+            $table->string('slug')->unique();
+            $table->text('icon')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
