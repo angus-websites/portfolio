133b7b0e8d2e1141736b4219d2838346e434084e
Angus Goody <angus.goody@gmail.com>
2021-12-23 23:12:10 +0000

    Added new navbar

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -1,87 +1,85 @@
-<nav x-data="{ open: false }"  class="bg-white shadow-md">
-  <div class="container mx-auto px-6 md:px-3 ">
-
-    <div class="relative flex items-center justify-between h-16">
-      
-      <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-        <div class="flex-shrink-0 flex items-center">
-          <img class="block lg:hidden h-8 w-auto" src="/assets/images/core/logo.svg" alt="Workflow">
-          <img class="hidden lg:block h-8 w-auto" src="/assets/images/core/logo.svg" alt="Workflow">
+<!--Navbar-->
+<nav x-data="{ open: false }" class="shadow-md bg-white">
+  <!--Desktop menu-->
+  <nav class="navbar">
+    <div class="container mx-auto px-6 md:px-3">
+      <div class="flex-none">
+        <img class="block lg:hidden h-8 w-auto" src="/assets/images/core/logo.svg" alt="Workflow">
+        <img class="hidden lg:block h-8 w-auto" src="/assets/images/core/logo.svg" alt="Workflow">
+      </div> 
+      <div class="flex-1 px-2 mx-2">
+        <div class="items-stretch hidden lg:flex">
+          <x-nav-link href="/" :active="Request::is('/')">
+            {{ __('Home') }}
+          </x-nav-link>
+          <x-nav-link href="/projects" :active="Request::is('projects')">
+            {{ __('Projects') }}
+          </x-nav-link>
+          <x-nav-link href="/contact" :active="Request::is('contact')">
+            {{ __('Contact') }}
+          </x-nav-link>
         </div>
-        <div class="hidden sm:block sm:ml-6">
-          <div class="flex space-x-4">
-            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="/" class="px-3 py-2 text-md font-medium rounded-md {{ Request::is('/') ? 'text-primary-focus' : 'text-gray-500 hover:bg-blue-100 hover:text-blue-600' }}" aria-current="page">Home</a>
+      </div> 
+      <div class="flex-none">
 
-            <a href="/projects" class="px-3 py-2 text-md font-medium rounded-md {{ Request::is('projects') ? 'text-primary-focus' : 'text-gray-500 hover:bg-blue-100 hover:text-blue-600' }}" aria-current="page">Projects</a>
-
-            <a href="/contact" class="px-3 py-2 text-md font-medium rounded-md {{ Request::is('contact') ? 'text-primary-focus' : 'text-gray-500 hover:bg-blue-100 hover:text-blue-600' }}" aria-current="page">Contact</a>
-
-          </div>
-        </div>
-      </div>
-      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static hidden sm:block">
         @if (Auth::check())
-          <x-dropdown align="right" width="48" class="sm:hidden">
-            <x-slot name="trigger">
-              <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                <div>{{ Auth::user()->name }}</div>
-                <div class="ml-1">
-                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                  </svg>
-                </div>
-              </button>
-            </x-slot>
-            <x-slot name="content">
-              <!--Other links-->
-              <x-dropdown-link href="/dashboard" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-              </x-dropdown-link>
-              <!-- Authentication -->
+          <!--Desktop dropdown--->
+          <div class="dropdown dropdown-end hidden lg:block">
+            <!--Trigger-->
+            <div tabindex="0" class="btn btn-link text-gray-500">
+                <span>{{ Auth::user()->name }}</span>
+                <svg class="fill-current h-4 w-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+            </div> 
+
+            <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 text-neutral rounded-box w-52">
+              <li>
+                <a href="/dashboard">Dashboard</a>
+              </li>
+              <li>
+                <a href="/account">Account</a>
+              </li> 
               <form method="POST" action="{{ route('logout') }}">
                 @csrf
-                <x-dropdown-link :href="route('logout')"
-                onclick="event.preventDefault();
-                this.closest('form').submit();">
-                {{ __('Log out') }}
-              </x-dropdown-link>
-            </form>
-          </x-slot>
-          </x-dropdown>
+                  <li>
+                    <a onclick="event.preventDefault(); this.closest('form').submit();">Logout</a>
+                  </li> 
+              </form>
+            </ul>
+          </div>
         @else
-          <a href="/login" class="px-3 py-2 text-md font-medium rounded-md {{ Request::is('login') ? 'text-green-600' : 'text-gray-500 hover:bg-green-100 hover:text-green-600' }}" aria-current="page">Login</a>
+          <x-nav-link href="/login" :active="Request::is('login')" class="hidden lg:flex">
+            {{ __('Login') }}
+          </x-nav-link>
         @endif
-      </div>
 
-      <!-- Hamburger-->
-      <div class="-mr-2 flex items-center sm:hidden">
-        <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+        <!--Hamburger-->
+        <button @click="open = ! open" class="lg:hidden btn btn-square btn-ghost hover:bg-gray-100 hover:text-gray-500 drawer-button" for="my-drawer">
           <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
             <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
-      </div>
+      </div> 
     </div>
-  </div>
-
+  </nav>
   <!-- Responsive Navigation Menu -->
-  <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-    <div class="pt-2 pb-3 space-y-1">
-      <x-responsive-nav-link href="/" :active="request()->routeIs('index')">
+  <div :class="{'block': open, 'hidden': ! open}" class="hidden lg:hidden w-full">
+    <ul class="menu p-4 overflow-y-auto">
+      <x-responsive-nav-link href="/" :active="Request::is('/')">
         {{ __('Home') }}
       </x-responsive-nav-link>
-      <x-responsive-nav-link href="/projects" :active="request()->routeIs('projects')">
+      <x-responsive-nav-link href="/projects" :active="Request::is('projects')">
         {{ __('Projects') }}
       </x-responsive-nav-link>
-      <x-responsive-nav-link href="/contact" :active="request()->routeIs('contact')">
+      <x-responsive-nav-link href="/contact" :active="Request::is('contact')">
         {{ __('Contact') }}
       </x-responsive-nav-link>
-    </div>
+    </ul>
 
     @if (Auth::check())
-      <!-- Responsive Settings Options -->
+      <!--User info-->
       <div class="pt-4 pb-1 border-t border-gray-200">
         <div class="flex items-center px-4">
           <div class="flex-shrink-0">
@@ -89,40 +87,33 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
             </svg>
           </div>
+          <div class="ml-3">
+            <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+            <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+          </div>
+        </div> 
+      </div>
 
-          
-            <div class="ml-3">
-              <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-              <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-        </div>
-
-        <div class="mt-3 space-y-1">
-          <!--Other links-->
-          <x-responsive-nav-link href="/dashboard" :active="request()->routeIs('dashboard')">
-            {{ __('Dashboard') }}
-          </x-responsive-nav-link>
-
-          <!-- Authentication -->
-          <form method="POST" action="{{ route('logout') }}">
-            @csrf
-
-            <x-responsive-nav-link :href="route('logout')"
-            onclick="event.preventDefault();
-            this.closest('form').submit();">
+      <ul class="menu p-4 overflow-y-auto">
+        <!--Dashboard link-->
+        <x-responsive-nav-link href="/dashboard" :active="Request::is('dashboard')">
+          {{ __('Dashboard') }}
+        </x-responsive-nav-link>
+        <!--Logout link-->
+        <form method="POST" action="{{ route('logout') }}">
+          @csrf
+          <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
             {{ __('Log out') }}
           </x-responsive-nav-link>
         </form>
-      </div>
+      </ul>
     @else
-      <div class="pt-2 border-t border-gray-200">
-        <!--Other links-->
-        <x-responsive-nav-link href="/login" :active="request()->routeIs('login')">
+      <ul class="menu p-4 overflow-y-auto border-t border-gray-200">
+        <x-responsive-nav-link href="/login" :active="Request::is('login')">
           {{ __('Login') }}
         </x-responsive-nav-link>
-      </div>
+      </ul>
     @endif
-
-  </div>
   </div>
 </nav>
+
