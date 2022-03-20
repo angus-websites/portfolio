760a75ecac180975c46704cc8f052df4d48079b2
Angus Goody <angus.goody@gmail.com>
2022-03-20 10:49:41 +0000

    Updating with alpine

--- resources/views/livewire/projects/create.blade.php
+++ resources/views/livewire/projects/create.blade.php
@@ -68,28 +68,56 @@
         <div class="p-4">
 
             <!--Github checkbox-->
-            <div class="form-control mb-4">
-                <div class="col-span-6 sm:col-span-4 flex items-start">
-                  <div class="flex items-center h-5">
-                    <input id="has_git" name="has_git" type="checkbox" class="checkbox checkbox-sm toggle_block">
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label for="has_git" class="font-medium text-gray-700">Github Link</label>
-                    <p class="text-gray-500">Does this project include a public github link?</p>
-                  </div>
+            <div x-data="{show: false}" class="mb-6" >
+                <div class="form-control">
+                    <div class="col-span-6 sm:col-span-4 flex items-start">
+                      <div class="flex items-center h-5">
+                        <input @click='show = !show' id="has_git" name="has_git" type="checkbox" class="checkbox checkbox-sm toggle_block">
+                      </div>
+                      <div class="ml-3 text-sm">
+                        <label for="has_git" class="font-medium text-gray-700">Github Link</label>
+                        <p class="text-gray-500">Does this project include a public github link?</p>
+                      </div>
+                    </div>
+                </div>
+                <div class="form-control" x-show="show" x-transition>
+                    <x-label for="git_link" :value="__('Github link')" />
+                    <x-input wire:model="project.git_link"
+                                id="git_link"
+                                class="input-bordered"
+                                type="url"
+                                name="git_link"
+                                error="project.git_link"
+                                showgreen="true"
+                                required />
                 </div>
             </div>
 
+
+
             <!-- Website link -->
-            <div class="form-control mb-4">
-                <div class="col-span-6 sm:col-span-4 flex items-start">
-                  <div class="flex items-center h-5">
-                    <input id="has_web" name="has_web" type="checkbox" class="checkbox checkbox-sm toggle_block">
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label for="has_web" class="font-medium text-gray-700">Website Link</label>
-                    <p class="text-gray-500">Does this project include a live public website?</p>
-                  </div>
+            <div class="mb-6" x-data="{show: false}">
+                <div class="form-control">
+                    <div class="col-span-6 sm:col-span-4 flex items-start">
+                      <div class="flex items-center h-5">
+                        <input @click='show = !show' id="has_web" name="has_web" type="checkbox" class="checkbox checkbox-sm toggle_block">
+                      </div>
+                      <div class="ml-3 text-sm">
+                        <label for="has_web" class="font-medium text-gray-700">Website Link</label>
+                        <p class="text-gray-500">Does this project include a live public website?</p>
+                      </div>
+                    </div>
+                </div>
+                <div class="form-control" x-show="show" x-transition:enter="transition ease-out duration-300" >
+                    <x-label for="web_link" :value="__('Website link')" />
+                    <x-input wire:model="project.web_link"
+                                id="web_link"
+                                class="input-bordered"
+                                type="url"
+                                name="web_link"
+                                error="project.web_link"
+                                showgreen="true"
+                                required />
                 </div>
             </div>
 
