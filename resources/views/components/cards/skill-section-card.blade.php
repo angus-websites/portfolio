22656d0296e651f92e2204c5c262583570dcb36e
Angus <angus.goody@gmail.com>
2022-04-23 22:28:19 +0100

    Adding new layout for skill section

--- resources/views/components/cards/skill-section-card.blade.php
+++ resources/views/components/cards/skill-section-card.blade.php
@@ -4,8 +4,9 @@
         <p class="uppercase text-lg font-bold mb-2 text-center">{{$name}}</p>
         <hr>
     </div>
+    <!-- Content for skills-->
     <div class="flex-1">
-        <table class="my-5 w-full">
+        {{-- <table class="my-5 w-full">
           <tbody>
             @if(sizeof($skills) < 1)
             <tr class="text-center">
@@ -16,10 +17,10 @@
             @endif
             @foreach($skills as $skill)
               <tr>
-                <td>
+                <td class="py-4">
                   <div class="flex items-center space-x-3">
                     <div class="avatar">
-                      <div class="w-12 h-12 mask mask-squircle">
+                      <div class="w-10">
                         <img src={{$skill->getIcon()}}>
                       </div>
                     </div>
@@ -40,7 +41,38 @@
               </tr>
             @endforeach
           </tbody>
-        </table>
+        </table> --}}
+        <!--Grid -->
+        <div class="flex flex-wrap my-5 gap-x-5 gap-y-8 justify-around" >
+          @forelse($skills as $skill)
+            <div>
+              <div class="flex flex-row items-center gap-x-3">
+                @if($skill->icon)
+                  <div class="avatar">
+                    <div class="w-8">
+                      <img src={{$skill->getIcon()}}>
+                    </div>
+                  </div>
+                @else
+                  <div class="avatar placeholder">
+                    <div class="bg-secondarylight-100 text-secondarylight-300 rounded-full w-8">
+                      <span class="text-xs">{{ucfirst($skill->name[0])}}</span>
+                    </div>
+                  </div>
+                @endif
+                <div class="flex-1 font-medium text-left">
+                  {{$skill->name}}
+                </div>
+              </div>
+            </div>
+          @empty
+            <div class="col-span-2 text-center">
+              <td class="font-medium">
+                  <div class="badge badge-outline">No skills found</div>
+              </td>
+            </div>
+          @endforelse
+        </div>
     </div>
     @can('update', App\Models\SkillSection::class)
         @if($editmode)
