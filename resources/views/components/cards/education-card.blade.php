43ad0dbffc9d534b6f868dbe906ff61d92df3618
Angus <angus.goody@gmail.com>
2022-05-04 21:57:34 +0100

    Fixed bug

--- resources/views/components/cards/education-card.blade.php
+++ resources/views/components/cards/education-card.blade.php
@@ -1,7 +1,7 @@
 
 @if($isCollapse() == true)
   <!--Education collapse-->
-  <div tabindex="0" class="collapse rounded-lg bg-base-200 collapse-arrow border">
+  <div class="collapse rounded-lg bg-base-200 collapse-arrow border">
     <input aria-hidden="true" tabindex="-1" type="checkbox" class="h-full">
     <div class="collapse-title">
       <div class="flex items-start space-x-5">
