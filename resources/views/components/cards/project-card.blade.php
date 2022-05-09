8654c0a820bcb2369076c8cb171b4e534cb6e331
Angus <angus.goody@gmail.com>
2022-05-09 21:25:05 +0100

    Added different layout to project card

--- resources/views/components/cards/project-card.blade.php
+++ resources/views/components/cards/project-card.blade.php
@@ -4,13 +4,14 @@
     <a href="{{$link}}">
       <div class="h-full bg-base-200 border rounded-lg overflow-hidden">
         <img class="lg:h-48 md:h-36 w-full object-cover object-center" height="60" src="{{$imagePath}}" alt="{{$getAlt()}}">
-        <div class="p-6 border-t-2 border-gray-100">
+        <div class="p-6 border-t-2 border-gray-100 flex-row space-y-4">
+          <h2 class="title-font text-lg font-medium text-gray-900">{{$title}}</h2>
           <div class="flex flex-row justify-between">
             <p class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{{$project->category()->short_name}}</p>
+            <div class="divider divider-horizontal"></div>
             <p class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{{$project->yearMade()}}</p>
           </div>
-          <h2 class="title-font text-lg font-medium text-gray-900 mb-3">{{$title}}</h2>
-          <p class="leading-relaxed mb-3">{{$description}}</p>
+          <p class="leading-relaxed">{{$description}}</p>
         </div>
       </div>
     </a>
