c7409a4005488f06248549ca28875d1737adfa7a
Angus <angus.goody@gmail.com>
2022-05-13 11:26:25 +0100

    Added new contact grid and error page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -18,28 +18,29 @@
     <form method="POST" action="/contact">
       @csrf
       <div class="lg:w-1/2 md:w-2/3 mx-auto">
-        <div class="flex flex-wrap -m-2">
-          <div class="p-2 w-1/2">
+
+        <!--Contact grid-->
+        <div class="grid grid-cols-2 gap-y-2 gap-x-4">
+          <div class="col-span-2 sm:col-span-1">
             <div class="form-control">
               <x-label for="name" :value="__('Name')" />
               <x-input name="name" type="text" class="input-bordered" placeholder="" required></x-input>
             </div>
           </div>
-          <div class="p-2 w-1/2">
+          <div class="col-span-2 sm:col-span-1">
             <div class="form-control">
               <x-label for="email" :value="__('Email')" />
               <x-input name="email" type="email" class="input-bordered" placeholder="" required></x-input>
             </div>
           </div>
-          <div class="p-2 w-full">
+
+          <div class="col-span-2">
             <div class="form-control">
                 <x-label for="message" :value="__('Your Message')" />
                 <textarea name="message" class="textarea textarea-bordered h-24" placeholder=""></textarea>
             </div>
           </div>
-          <div class="p-2 w-full mt-4">
-            <x-button class="btn-primary btn-block">Send</x-button>
-          </div>
+
         </div>
       </div>
     </form>
