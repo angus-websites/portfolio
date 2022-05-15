e15d37adf9c0c450becc4af9b053b436e241fd30
Angus <angus.goody@gmail.com>
2022-05-15 18:13:37 +0100

    Added alert component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,5 +5,8 @@
 <x-app-layout>
   <x-page-container>
     <x-page-title title="About" subtitle="About this site"/>
+    <div class="my-20">
+      <a target="_blank" href="https://www.ecowebhosting.co.uk/" alt="Planting trees every month with Eco Web Hosting" rel="noopener"><img src="https://eco-cdn.co.uk/eco-badge-1.svg" alt="Planting trees every month with Eco Web Hosting"></a>
+    </div>
   </x-page-container>
 </x-app-layout>
--- resources/views/livewire/skills/edit.blade.php
+++ resources/views/livewire/skills/edit.blade.php
@@ -1,14 +1,6 @@
 <div>
-    @if (session()->has('success'))
-        <div class="alert alert-success shadow-lg mb-5">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-            <span>{{ session('success') }}</span>
-          </div>
-        </div>
-    @endif
+    <x-alerts.all />
 
-   
     <form wire:submit.prevent="saveSkill">
         <div class="grid grid-cols-6">
 
