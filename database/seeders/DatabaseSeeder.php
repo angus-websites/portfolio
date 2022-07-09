cbc5e0fb6ae00089f79ed5c29e7f1cf86089f320
Angus <angus.goody@gmail.com>
2022-07-09 15:50:16 +0100

    Updated Readme and env

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,14 +7,23 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Seed the application's database
+     * for developer purposes
      *
      * @return void
      */
     public function run()
     {
       // \App\Models\User::factory(10)->create();
+      $this->call(CategorySeeder::class);
+      $this->call(TagSeeder::class);
+      $this->call(ProjectSeeder::class);
       $this->call(RoleSeeder::class);
       $this->call(UserSeeder::class);
+      $this->call(SkillSectionSeeder::class);
+      $this->call(SkillSeeder::class);
+      $this->call(EducationSeeder::class);
+      $this->call(EmploymentSeeder::class);
+      $this->call(BlockSeeder::class);
     }
 }
