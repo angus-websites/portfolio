0911b596cae10ea96c862248a02f08d662a40471
Angus <angus.goody@gmail.com>
2023-01-15 00:55:38 +0000

    Another tweak

--- resources/views/components/cards/skill-section-card.blade.php
+++ resources/views/components/cards/skill-section-card.blade.php
@@ -25,7 +25,7 @@
       @forelse($skills as $skill)
         <!-- Each skill -->
         <div class="pt-5">
-          <div class="flow-root rounded-lg sm:bg-gray-50 sm:border px-6 pb-8">
+          <div class="flow-root rounded-lg px-6 pb-8">
             <div class="-mt-6">
               <div class="">
                 <div class="inline-flex items-center justify-center rounded-xl bg-white p-2 shadow-lg border">
@@ -35,7 +35,7 @@
                   </div>
                   @else
                     <span class="inline-flex h-10 w-10 items-center justify-center">
-                      <span class="text-xs font-medium leading-none">{{ strtoupper(substr($skill->name, 0, 1)) }}</span>
+                      <span class="text-xs sm:text-sm font-medium leading-none">{{ strtoupper(substr($skill->name, 0, 1)) }}</span>
                     </span>
                   @endif
                 </div>
@@ -47,37 +47,4 @@
       @endforeach
 
     </div>
-    {{-- <div class="flex-1">
-        <!--Grid -->
-        <div class="flex flex-wrap gap-x-5 gap-y-8 justify-around my-5" >
-          @forelse($skills as $skill)
-            <div class="basis-full sm:basis-1/3">
-              <div class="flex flex-row justify-center items-center space-x-3">
-                @if($skill->icon)
-                  <div class="avatar">
-                    <div class="w-8 rounded-full">
-                      <img src={{$skill->getIcon()}}>
-                    </div>
-                  </div>
-                @else
-                  <div class="avatar placeholder">
-                    <div class="bg-secondarylight-100 text-secondarylight-300 rounded-full w-8">
-                      <span class="text-xs">{{ucfirst($skill->name[0])}}</span>
-                    </div>
-                  </div>
-                @endif
-                <div class="flex-1 sm:flex-none font-medium text-left">
-                  {{$skill->name}}
-                </div>
-              </div>
-            </div>
-          @empty
-            <div class="col-span-2 text-center">
-              <td class="font-medium">
-                  <div class="badge badge-outline">No skills found</div>
-              </td>
-            </div>
-          @endforelse
-        </div>
-    </div> --}}
   </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
 
   <div class="mb-40 mx-auto max-w-7xl px-4 sm:px-6">
 
-    <div class="grid-cols-1 space-y-32">
+    <div class="grid-cols-1 sm:space-y-32 space-y-20">
     
       <div id="employmentEducationSection" class="grid grid-cols-1 gap-y-10 gap-x-10 lg:grid-cols-2">
         @if(count($employment) > 0)
