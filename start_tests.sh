6582f928474144c6d910c5baf2833034baa6d689
Angus <angus.goody@gmail.com>
2023-08-07 18:44:54 +0100

    Added CI/CD files

---
+++ start_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Create .env file from environment variables
+#printenv | awk -F "=" 'NF==2 && $2 !~ /[\n\t ]/' > .env
+
+# Run our artisan commands
+php artisan route:clear
+php artisan config:clear
+php artisan view:clear
+
+php artisan storage:link
+
+php artisan migrate --force
+
+php artisan db:seed --force
+
+php artisan test
