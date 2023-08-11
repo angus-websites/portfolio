e620200cc30b2008f13b0724f0e07055b7516597
Angus <angus.goody@gmail.com>
2023-08-11 23:58:12 +0100

    not running in d mode

--- start_prod.sh
+++ start_prod.sh
@@ -3,9 +3,6 @@
 # Create .env file from environment variables
 printenv | awk -F "=" 'NF==2 && $2 !~ /[\n\t ]/' > .env
 
-# Ensure php-fpm runs in the web group
-sed -i 's/^group =.*/group = web/' /usr/local/etc/php-fpm.d/www.conf
-
 # Run our artisan commands
 php artisan route:clear
 php artisan config:clear
